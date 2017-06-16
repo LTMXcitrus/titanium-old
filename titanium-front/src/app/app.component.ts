a7f6aa92ba88ae46830e805acbac7f34a4c0bc90
matthieu_lemonnier <mlemonnier@voyages-sncf.com>
2017-06-16 18:03:06 +0200

    handle click events on welcome component

--- titanium-front/src/app/app.component.ts
+++ titanium-front/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Stock} from "./model/stock";
+import {TitaniumService} from "./titanium.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private titaniumService: TitaniumService){}
+
+  stock: Stock;
+
+  enter(stockName: string){
+    this.titaniumService.getStock(stockName);
+  }
 }
--- titanium-front/src/app/welcome/welcome.component.ts
+++ titanium-front/src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -10,6 +10,13 @@ export class WelcomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @Output()
+  enterApp: EventEmitter<string> = new EventEmitter();
+
+  enter(stockName: string){
+    this.enterApp.emit(stockName);
   }
 
 }
